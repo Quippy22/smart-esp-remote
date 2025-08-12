fe0c0b8d865051947139e0f7a06044afe8d51c3a
darius alexandru <darius.alexandru2202@gmail.com>
2025-08-12 20:32:06 +0300

    added a helper class to store display information

--- lib/screens.py
+++ lib/screens.py
@@ -4,6 +4,28 @@ from machine import I2C, Pin, Timer
 
 from ssd1306 import SSD1306_I2C
 
-i2c = I2C(sda=Pin(27), scl=Pin(26))
-oled = SSD1306_I2C(128, 64, i2c)
 
+
+class Display:
+    def __init__(self, sda, scl, width, height):
+        self.sda = sda
+        self.scl = scl
+        self.width = width
+        self.height = height
+
+        self.i2c = I2C(sda=Pin(self.sda), scl=Pin(self.scl))
+        self.oled = SSD1306_I2C(width, height, self.i2c)
+            
+    def clear(self):
+        self.oled.fill(0)
+        self.oled.show()
+
+class MenuScreen(Display):
+    def __init__(self, sda, scl, width, height):
+        super().__init__(sda, scl, width, height)
+        pass
+
+class TypingScreen(Display):
+    def __init__(self, sda, scl, width, height):
+        super().__init__(sda, scl, width, height)
+        pass
